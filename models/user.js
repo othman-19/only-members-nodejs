8b52bbb889018f7dfaff19d38968f0f8374cdc84
othmane <othman-19@hotmail.fr>
2020-10-29 20:33:00 +0100

    install the jsonwebtoken and added the application secret

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.SECRET;
 
 const { Schema } = mongoose;
 
