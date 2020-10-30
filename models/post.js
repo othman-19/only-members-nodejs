901b89c96fb250d247cb3473f4ffaefb3505bc96
othmane <othman-19@hotmail.fr>
2020-10-30 11:13:09 +0100

    add the post model

---
+++ models/post.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const PostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "can't be blank"],
+      maxlength: 200,
+    },
+    text: {
+      type: String,
+      required: [true, "can't be blank"],
+      maxlength: 300,
+    },
+    Author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, "can't be blank"],
+    },
+  },
+  { timestamps: true },
+);
+
+PostSchema.virtual('url').get(function () {
+  return `/post/${this._id}`;
+});
+
+module.exports = mongoose.model('Post', PostSchema);
