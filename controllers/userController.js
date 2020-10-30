d526d296c7d58a0115caaf9604c7f010d1513f08
othmane <othman-19@hotmail.fr>
2020-10-30 14:27:48 +0100

    add the user show method

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,3 +17,35 @@ exports.index = (req, res, next) => {
       });
     });
 };
+
+// Display detail page for a specific user.
+exports.show = (req, res, next) => {
+  async.parallel(
+    {
+      user(callback) {
+        User.findById(req.params.id).exec(callback);
+      },
+
+      userPosts(callback) {
+        Post.find({ author: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.user === null) {
+        // No results.
+        const error = new Error('User not found');
+        error.status = 404;
+        return next(error);
+      }
+      // Successful, so render
+      return res.render('/user/show', {
+        title: 'User Detail',
+        user: results.user,
+        userPosts: results.userPosts,
+      });
+    },
+  );
+};
