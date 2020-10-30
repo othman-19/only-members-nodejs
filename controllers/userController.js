0c2b1f3941a6fba47b460b413b5d59d2342ad080
othmane <othman-19@hotmail.fr>
2020-10-30 14:15:06 +0100

    create index function in user controller, install express-validator package

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,3 +3,17 @@ const { body, validationResult } = require('express-validator');
 const User = require('../models/user');
 const Post = require('../models/post');
 
+// Display list of all users.
+exports.index = (req, res, next) => {
+  User.find({}, "full_name")
+    .exec((err, userList) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful, so render
+      return res.render("user_list", {
+        title: "Users List",
+        userList,
+      });
+    });
+};
