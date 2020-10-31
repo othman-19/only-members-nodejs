b42af1f9c5a01ac3998325433847c066a02dff3f
othmane <othman-19@hotmail.fr>
2020-10-31 19:35:32 +0100

    require express-ejs-layouts in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const Layouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
