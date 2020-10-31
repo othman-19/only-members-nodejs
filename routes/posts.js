e7370f7bca8c6968e3e3034aff66d25cddd255c6
othmane <othman-19@hotmail.fr>
2020-10-31 17:28:31 +0100

    change thename of controller in routes/posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,27 +1,26 @@
 const PostRouter = require('express').PostRouter();
+const postController = require('../controllers/postController');
 
-// GET request for creating a post. NOTE This must come before route that displays Genre (uses id).
-PostRouter.get('/post/create', post_controller.post_new);
+// GET request for creating a record.
+// NOTE This must come before route that displays Genre (uses id).
+PostRouter.get('/post/create', postController.new);
 
-// POST request for creating post.
-PostRouter.post('/post/create', post_controller.post_create);
+// POST request for creating record.
+PostRouter.post('/post/create', postController.create);
 
-// GET request to delete post.
-PostRouter.get('/post/:id/delete', post_controller.post_delete_get);
+// POST request to delete record.
+PostRouter.post('/post/:id/delete', postController.delete);
 
-// POST request to delete post.
-PostRouter.post('/post/:id/delete', post_controller.post_delete);
+// GET request to update record.
+PostRouter.get('/post/:id/update', postController.edit);
 
-// GET request to update post.
-PostRouter.get('/post/:id/update', post_controller.post_edit);
+// POST request to update record.
+PostRouter.post('/post/:id/update', postController.update);
 
-// POST request to update post.
-PostRouter.post('/post/:id/update', post_controller.post_update);
+// GET request for one record.
+PostRouter.get('/post/:id', postController.show);
 
-// GET request for one post.
-PostRouter.get('/post/:id', post_controller.post_show);
-
-// GET request for list of all posts.
-PostRouter.get('/', post_controller.post_index);
+// GET request for list of all records.
+PostRouter.get('/', postController.index);
 
 module.exports = PostRouter;
--- routes/users.js
+++ routes/users.js
@@ -2,25 +2,25 @@ const UserRouter = require('express').UserRouter();
 
 const userController = require('../controllers/userController');
 
-// GET request for creating a user. NOTE This must come before routes that display user.
+// GET request for creating a record. NOTE This must come before routes that display user.
 UserRouter.get('/user/create', userController.new);
 
-// POST request for creating user.
+// POST request for creating record.
 UserRouter.post('/user/create', userController.create);
 
-// DELETE request to delete user.
+// DELETE request to delete record.
 UserRouter.delete('/user/:id', userController.delete);
 
-// GET request to update user.
+// GET request to update record.
 UserRouter.get('/user/:id/update', userController.edit);
 
-// POST request to update user.
+// POST request to update record.
 UserRouter.post('/user/:id/update', userController.update);
 
-// GET request for one user.
+// GET request for one record.
 UserRouter.get('/user/:id', userController.show);
 
-// GET request for list of all users.
+// GET request for list of all records.
 UserRouter.get('/users', userController.index);
 
 module.exports = UserRouter;
