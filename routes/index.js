e484fe245527b40542599a22d6ad7e6d8c961828
othmane <othman-19@hotmail.fr>
2020-10-31 19:25:57 +0100

    install the express-ejs-layouts package

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.redirect('/posts');
+  res.render('index', { title: 'posts' });
 });
 
 module.exports = router;
