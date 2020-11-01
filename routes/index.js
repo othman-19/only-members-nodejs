aebf8733df1e0adf101a611d4b2f74b206c7e552
othmane <othman-19@hotmail.fr>
2020-11-01 18:52:08 +0100

    create two route for login and register in the routers/index form

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,13 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'posts' });
 });
 
+/* GET login form. */
+router.get('/login', (req, res, next) => {
+  res.render('login', { title: 'Log In' });
+});
+/* GET register form. */
+router.get('/register', (req, res, next) => {
+  res.render('register', { title: 'Register' });
+});
+
 module.exports = router;
