b1183e11019651a5dedf7d9094856732dca2a588
othmane <othman-19@hotmail.fr>
2020-11-10 20:09:48 +0100

    check authentication before registration, login and membership routes

--- routes/index.js
+++ routes/index.js
@@ -11,24 +11,24 @@ router.get('/', (req, res, next) => {
 });
 
 /* GET login form. */
-router.get('/login', (req, res, next) => {
+router.get('/login', authentications.checkNotAuthenticatedUser, (req, res, next) => {
   res.render('login', { title: 'Log In' });
 });
 
-router.post('/login',
+router.post('/login', authentications.checkNotAuthenticatedUser,
   passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
   }));
 /* GET register form. */
-router.get('/register', (req, res, next) => {
+router.get('/register', authentications.checkNotAuthenticatedUser, (req, res, next) => {
   res.redirect('users/create');
 });
 
 /* GET membership form. */
-router.get('/membership', authentications.authenticateNotMember, userRoleController.newMember);
+router.get('/membership', authentications.checkAuthenticatedUser, authentications.authenticateNotMember, userRoleController.newMember);
 
 /* Post membership form. */
-router.post('/membership', authentications.authenticateNotMember, userRoleController.createMember);
+router.post('/membership', authentications.checkAuthenticatedUser, authentications.authenticateNotMember, userRoleController.createMember);
 
 module.exports = router;
