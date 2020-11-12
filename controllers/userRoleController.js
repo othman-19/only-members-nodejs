ca013430dc92e4d98af0e99ec764fd0a6b1bbf11
othmane <othman-19@hotmail.fr>
2020-11-12 21:53:58 +0100

    change the userRoleController create member function to be convinient with User new role attribute

--- controllers/userRoleController.js
+++ controllers/userRoleController.js
@@ -2,10 +2,10 @@ const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 // Display record new form.
-exports.newMember = (req, res, next) => res.render('user/membershipForm', { title: 'Become a member' });
+exports.newMember = (req, res, next) => res.render('user/RoleForm', { title: 'Become a member' });
 
-exports.membershipValidations = [
-  check('memberPassword')
+exports.roleValidations = [
+  check('rolePassword')
     .isLength({ min: 8 })
     .withMessage('Password Must Be at Least 8 Characters')
     .matches('[0-9]')
@@ -15,7 +15,7 @@ exports.membershipValidations = [
     .trim()
     .escape(),
 
-  check('memberPasswordConfirmation', 'passwordConfirmation field must have the same value as the password field')
+  check('rolePasswordConfirmation', 'passwordConfirmation field must have the same value as the password field')
     .exists()
     .custom((value, { req }) => value === req.body.password),
 ];
@@ -27,13 +27,13 @@ exports.createMember = (req, res, next) => {
 
   // Create a record object with validated data.
   const data = {
-    memberPassword: req.body.memberPassword,
-    memberPasswordConfirmation: req.body.memberPasswordConfirmation,
+    rolePassword: req.body.rolePassword,
+    rolePasswordConfirmation: req.body.rolePasswordConfirmation,
   };
 
   if (!errors.isEmpty()) {
     // There are errors. Render the form again with sanitized values/error messages.
-    res.render('user/membershipForm', {
+    res.render('user/roleForm', {
       title: 'Become a member',
       data,
       errors: errors.array(),
@@ -41,12 +41,12 @@ exports.createMember = (req, res, next) => {
   } else {
     // Data from form is valid.
     // eslint-disable-next-line consistent-return
-    bcrypt.hash(req.body.memberPassword, 10, (err, hashed) => {
+    bcrypt.hash(req.body.rolePassword, 10, (err, hashed) => {
       if (err) {
         return next(err);
       }
-      currentUser.membership.pass = hashed;
-      currentUser.currentUser.membership.status = true;
+      currentUser.role.memberPass = hashed;
+      currentUser.role = 'member';
       // data saved. Redirect to record url.
       return res.redirect(currentUser.url);
     });
