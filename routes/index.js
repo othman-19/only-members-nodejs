92349ece3efba7c60e89af902ab21c668d648250
othmane <othman-19@hotmail.fr>
2020-11-12 22:22:02 +0100

    create get and post routes for admin

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const passport = require('passport');
-const userRoleController = require('../controllers/userRoleController');
+const userRoleController = require('../controllers/roleController');
 
 const { checkAuthenticatedUser } = require('../config/authentications');
 const { checkNotAuthenticatedUser } = require('../config/authentications');
 const { isNotMember } = require('../config/authorisations');
+const { isNotAdmin } = require('../config/authorisations');
 
 const router = express.Router();
 
@@ -28,10 +29,11 @@ router.get('/register', checkNotAuthenticatedUser, (req, res, next) => {
   res.redirect('users/create');
 });
 
-/* GET membership form. */
-router.get('/membership', checkAuthenticatedUser, isNotMember, userRoleController.newMember);
-
-/* Post membership form. */
-router.post('/membership', checkAuthenticatedUser, isNotMember, userRoleController.createMember);
+/* GET role form. */
+router.get('/member', checkAuthenticatedUser, isNotMember, userRoleController.newMember);
+router.get('/admin', checkAuthenticatedUser, isNotAdmin, userRoleController.newAdmin);
+/* Post role form. */
+router.post('/member', checkAuthenticatedUser, isNotMember, userRoleController.createMember);
+router.post('/admin', checkAuthenticatedUser, isNotAdmin, userRoleController.createAdmin);
 
 module.exports = router;
