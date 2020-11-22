a85e4c32d91c152339d4e97205c3bce0df7e62cb
othmane <othman-19@hotmail.fr>
2020-11-22 19:43:40 +0100

    add script for delete notifications, fix wrap prblem

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -18,3 +18,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach($delete => {
+    const $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
