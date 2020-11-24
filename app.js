a5a6b4d95d62986904f4084a2231a74aed62104c
othmane <othman-19@hotmail.fr>
2020-11-24 23:05:37 +0100

    disabled cors

--- app.js
+++ app.js
@@ -92,20 +92,20 @@ app.use(session({
 }));
 
 const allowedOrigins = ['null', 'http://localhost:3000', 'https://members-only-node.herokuapp.com'];
-app.use(cors({
-  origin(origin, callback) {
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not '
-                + 'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-}));
+// app.use(cors({
+//   origin(origin, callback) {
+//     // allow requests with no origin
+//     // (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not '
+//                 + 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true,
+// }));
 
 const csrfProtection = csrf({ cookie: true });
 app.use(csrfProtection);
