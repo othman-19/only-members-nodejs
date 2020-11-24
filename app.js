3c7390db3803e6720188d39ebb411c03e489feac
othmane <othman-19@hotmail.fr>
2020-11-24 20:45:13 +0100

    disable the content security option in helmet

--- app.js
+++ app.js
@@ -54,22 +54,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
 app.use(compression());
 
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: {
+//       directives: {
+//         defaultSrc: ["'self'"],
+//         scriptSrc: ["'self'"],
+//         fontSrc: ["'self'", 'https://fonts.googleapis.com/css?family=Lato:300,400,700,900'],
+//         styleSrc: [
+//           "'self'",
+//           'https://fonts.googleapis.com/css?family=Lato:300,400,700,900',
+//           'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css',
+//           'https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css',
+//         ],
+//         upgradeInsecureRequests: [],
+//       },
+//     },
+//   }),
+// );
+
 app.use(
   helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'"],
-        fontSrc: ["'self'", 'https://fonts.googleapis.com/css?family=Lato:300,400,700,900'],
-        styleSrc: [
-          "'self'",
-          'https://fonts.googleapis.com/css?family=Lato:300,400,700,900',
-          'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css',
-          'https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css',
-        ],
-        upgradeInsecureRequests: [],
-      },
-    },
+    contentSecurityPolicy: false,
   }),
 );
 
